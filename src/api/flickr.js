d420663c6ec18890f035ee3fac0f1352ac412179
Sam Wood <sam.wood@itech.media>
2019-04-23 12:33:30 +0100

    Refactor - Changed getRecent function to generic api request function with params

--- src/api/flickr.js
+++ src/api/flickr.js
@@ -3,20 +3,24 @@ import axios from 'axios';
 const apiKey = 'f5a29b2836a216f4ae14e88ec762fc3d';
 
 export default {
-
-    getRecent() {
-        return axios.get('https://api.flickr.com/services/rest', {
-            params: {
-              format: 'json',
-              method: 'flickr.photos.getRecent',
-              api_key: apiKey,
-              nojsoncallback: 1,
-              per_page: 25,
-            }
-          })
-          .catch((error) => {
-            // eslint-disable-next-line
-            console.log(error);
-          });
-    }
-}
+    sendRequest(method, options = {}) {
+        return axios
+            .get('https://api.flickr.com/services/rest', {
+                params: {
+                    ...{
+                        format: 'json',
+                        safe_search: 3,
+                        method: `flickr.photos.${method}`,
+                        api_key: apiKey,
+                        nojsoncallback: 1,
+                        per_page: 25,
+                    },
+                    ...options,
+                },
+            })
+            .catch(error => {
+                // eslint-disable-next-line
+                console.log(error);
+            });
+    },
+};
